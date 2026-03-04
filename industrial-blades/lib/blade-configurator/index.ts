753ab5c16f17839441548baa8ead9dbf8fe2b819
hüseyin oğulcan demir <demirhuseyinogulcan@gmail.com>
2026-03-04 17:25:36 +0300

    remove: Blade Configurator projesi tamamen kaldirildi
    - Tum blade-configurator bilesenler silindi (7 dosya)
    - blade-engineer sayfa rotasi silindi
    - bladeConfigurator sozluk girisleri 5 dilden temizlendi
    - Proje iptal edildi

--- industrial-blades/lib/blade-configurator/index.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Blade Configurator - Barrel Export
- */
-
-export * from './types';
-export { CATEGORIES, getDimensionFields } from './categories';
-export type { CategoryMeta, DimensionField } from './categories';
-export { exportBladeSpecPdf } from './pdf-spec';
